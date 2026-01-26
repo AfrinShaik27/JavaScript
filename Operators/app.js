2eb6bedd991e721ef10efc4bc8f6cfc48751847b
afrin shaik <skafrin7013@gmail.com>
2026-01-26 23:23:56 +0530

    Arithmetic Operators

---
+++ Operators/app.js
@@ -0,0 +1,26 @@
+//operators
+//operators are used to perform some operation on data
+//there are different types operators are avilable in javascript we will one by one
+//1.arithmetic operators
+//+,-,*./,%,**,++,--
+let a=5;
+let b=2;
+console .log("a + b =", a+b);//7
+console .log("a - b =", a-b);//3
+console .log("a * b =", a*b);//10
+console .log("a / b =", a/b);//2.5
+console .log("a % b =", a%b);//1
+console .log("a +** b =", a**b);//25
+// unary operators
+//postincrement first use then icrement
+console.log("a++ =",a++);
+console.log(a)
+//preincrement ++a;//first increment then use
+console.log("++a =",++a);
+console.log(a);
+//postdecrement b--: first use then decrement
+// console.log("b-- =",b--);
+// console.log(b);
+//predecrement --b :first decrement then use
+console.log("--b =",--b);
+console.log(b);
